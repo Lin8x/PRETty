cf7f2a0968ef3bf1b5a16fac4d42673e10b60a58
Lin8x <40849996+Lin8x@users.noreply.github.com>
2020-02-18 12:00:14 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,1 @@
+sudo apt -y install imagemagick ghostscript arp-scan tshark && sudo pip install -U argparse termcolor colorama pysnmp && git clone https://github.com/RUB-NDS/PRET && cd PRET && git clone https://github.com/BusesCanFly/PRETty && cd PRETty && chmod +x PRETty.py
